c9833c675045427e5d830c2c17995231ea050a8f
Cao Tri DO <cao-tri.do@keyrus.com>
2025-09-25 23:41:34 +0200

    feat: add running on databricks session and not only spark in demo

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,10 +1,18 @@
 """Demo script to showcase hotel_resa package functionality."""
+# To run this script, use:
+# DATABRICKS_CONFIG_PROFILE=cohort4 uv run scripts/demo.py
 
 # %% Databricks notebook source
 import pretty_errors  # noqa: F401
 from pyspark.sql import SparkSession
+from databricks.connect import DatabricksSession
+from loguru import logger
 
-spark = SparkSession.builder.getOrCreate()
+try:
+    spark = SparkSession.builder.getOrCreate()
+except:
+    logger.warning("Falling back to DatabricksSession")
+    spark = DatabricksSession.builder.getOrCreate()
 
 df = spark.read.table("samples.nyctaxi.trips")
 df.show(5)
