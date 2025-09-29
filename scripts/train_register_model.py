714314e535c24f9953fbfe23cbb2a864fad57ec2
Cao Tri DO <cao-tri.do@keyrus.com>
2025-09-29 11:40:37 +0200

    feat: add scripts to train, log experiment and register model in UC

---
+++ scripts/train_register_model.py
@@ -0,0 +1,65 @@
+"""Main entry point for train a model and register to MLFlow on hotel reservation data."""
+
+# Databricks notebook source
+
+import os
+
+import mlflow
+import pretty_errors  # noqa: F401
+from dotenv import load_dotenv
+from loguru import logger
+from pyspark.sql import SparkSession
+
+from mlops_course.marvelous.common import is_databricks
+from mlops_course.model.basic_model import BasicModel
+from mlops_course.utils.config import ProjectConfig, Tags
+from mlops_course.utils.databricks_utils import create_spark_session
+
+## COMMAND ----------
+# Global user setup 
+
+ENV_FILE = "./.env"
+CONFIG_FILE = "./project_config.yml"
+ENVIRONMENT_CHOICE="dev"
+
+# COMMAND ----------
+if not is_databricks():
+    load_dotenv(dotenv_path=ENV_FILE)
+    profile = os.getenv("PROFILE") # os.environ["PROFILE"]
+    mlflow.set_tracking_uri(f"databricks://{profile}")
+    mlflow.set_registry_uri(f"databricks-uc://{profile}")
+
+logger.info(f"MLflow Tracking URI: {mlflow.get_tracking_uri()}")
+logger.info(f"MLflow Registry URI: {mlflow.get_registry_uri()}")
+
+# COMMAND ----------
+config = ProjectConfig.from_yaml(config_path=CONFIG_FILE, env=ENVIRONMENT_CHOICE)
+# spark = SparkSession.builder.getOrCreate()
+spark = create_spark_session()
+
+tags_dict = {"git_sha": "abcd12345", "branch": "week2", "job_run_id": ""}
+tags = Tags(**tags_dict)
+
+# COMMAND ----------
+# Initialize model
+# Initialize model with the config path
+basic_model = BasicModel(config=config, tags=tags, spark=spark)
+logger.info("Model initialized.")
+
+# COMMAND ----------
+# Load data and prepare features
+basic_model.load_data()
+basic_model.prepare_features()
+logger.info("Loaded data, prepared features.")
+
+# COMMAND ----------
+# Train + log the model (runs everything including MLflow logging)
+basic_model.train()
+basic_model.log_model()
+logger.info("Model training completed.")
+
+# COMMAND ----------
+basic_model.register_model()
+logger.info("Registered model")
+
+# COMMAND ----------
