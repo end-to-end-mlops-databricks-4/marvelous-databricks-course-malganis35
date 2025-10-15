06b0d430fb69d769ccf5a9be1744af6a88da1912
Cao Tri DO <cao-tri.do@keyrus.com>
2025-10-15 22:48:02 +0200

    style(ruff): correct formatting ruff

--- scripts/05.refresh_monitor.py
+++ scripts/05.refresh_monitor.py
@@ -1,9 +1,10 @@
 import argparse
 
-from databricks.connect import DatabricksSession
 from databricks.sdk import WorkspaceClient
+from loguru import logger
 
 from hotel_reservation.utils.config import ProjectConfig
+from hotel_reservation.utils.databricks_utils import create_spark_session
 from hotel_reservation.visualization.monitoring import create_or_refresh_monitoring
 
 parser = argparse.ArgumentParser()
@@ -30,7 +31,11 @@ config_path = f"{root_path}/files/project_config.yml"
 # Load configuration
 config = ProjectConfig.from_yaml(config_path=config_path, env=args.env)
 
-spark = DatabricksSession.builder.getOrCreate()
+logger.info("Starting a Spark Session")
+spark = create_spark_session()
+
+logger.info("Starting a Workspace Session")
 workspace = WorkspaceClient()
 
+logger.info("Refresh Monitoring")
 create_or_refresh_monitoring(config=config, spark=spark, workspace=workspace)
